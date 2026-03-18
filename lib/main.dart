38ab2db1d9be36e251d93a43f97fed64e0dcf851
athallaabhi <athallaabhi@gmail.com>
2026-03-18 10:09:05 +0700

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,128 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: const Text('My App'), // Text
-        ), // AppBar
-        body: Center(
-          child: Text(
-            'Henshin',
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 60,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-              color: const Color.fromARGB(255, 43, 218, 31),
-            ), // TextStyle
-          ), // Text
-        ), // Center
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          backgroundColor: Colors.amber,
-          onPressed: () {},
-        ), // FloatingActionButton
-      ), // Scaffold
-    ),
-  ); // MaterialApp
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const RowColumnPage(),
+    );
+  }
+}
+
+class RowColumnPage extends StatelessWidget {
+  const RowColumnPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double screenWidth = mediaQueryData.size.width;
+    double screenHeight = mediaQueryData.size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bismillah', style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.orange[200],
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0),
+                padding: EdgeInsets.all(20.0),
+                color: Colors.lightBlue[100],
+                child: Center(
+                  child: Image.network(
+                    'https://tse1.mm.bing.net/th/id/OIP.z7hczHvzGF5s2S7UYcaruwHaJT?rs=1&pid=ImgDetMain&o=7&rm=3',
+                    fit: BoxFit.cover,
+                    width: 500,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.pink[200],
+            child: Text('HIDUP HIDUP HIDUP', style: TextStyle(fontSize: 16)),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(20.0),
+            margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+            color: Colors.yellow[200],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Column(children: [Icon(Icons.fastfood), Text("Food")]),
+                Column(children: [Icon(Icons.landscape), Text("Scenery")]),
+                Column(children: [Icon(Icons.people), Text("People")]),
+              ],
+            ),
+          ),
+          CounterCard(),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterCard extends StatefulWidget {
+  const CounterCard({super.key});
+
+  @override
+  State<CounterCard> createState() => _CounterCardState();
+}
+
+class _CounterCardState extends State<CounterCard> {
+  int _counter = 0; // This is the state (data) that changes.
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++; // Update the state.
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+      padding: EdgeInsets.all(20.0),
+      width: MediaQuery.of(context).size.width,
+      color: Colors.cyan[100],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Counter here: $_counter", style: TextStyle(fontSize: 16)),
+          Container(
+            color: Colors.cyan[200],
+            padding: EdgeInsets.all(5.0),
+            child: IconButton(
+              onPressed: _incrementCounter,
+              icon: Icon(Icons.add, color: Colors.black, size: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
